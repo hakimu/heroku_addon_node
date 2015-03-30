0203071ce119448f2fe26595eea2557dacc8cb04
Hakimu Jackson <hakimu@newrelic.com>
2015-03-29 19:41:24 -0700

    Added newrelic config

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['heroku_addon_node'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '325838f9e5ce0d0ddbaf427152d462c9a98560e9',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
